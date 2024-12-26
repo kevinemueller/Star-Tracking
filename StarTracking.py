580336e4284489554e9840a78933efb543c3566d
Kevin M. <64878242+kevinemueller@users.noreply.github.com>
2024-12-25 22:58:33 -0600

    Update StarTracking.py

--- StarTracking.py
+++ StarTracking.py
@@ -1,12 +1,10 @@
 import cv2
 import numpy as np
 
-#cam = cv2.VideoCapture('./test_video.mp4')
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(1)
 
 def nothing(x):
     pass
-
 
 cv2.namedWindow('Attributes')
 cv2.createTrackbar('R', 'Attributes', 0, 255, nothing)
@@ -34,17 +32,14 @@ class CameraFeed:
             if ret:
                 self.trackers = self.add_trackers(image=frame)
 
-                if self.trackers:
+                while self.trackers:
+                    tracker = self.trackers.pop()
+                    x, y, w, h = tracker['pos']
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 191, 255), 2)
 
-                    for tracker in self.trackers:
-                        x, y, w, h = tracker['pos']
-                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
+            cv2.imshow('Live', frame)
+            cv2.waitKey(1)
 
-                    cv2.imshow('Live', frame)
-                    cv2.waitKey(1)
-
-            #else:
-                #cam = cv2.VideoCapture('./test_video.mp4')
     
     def update_frame(self):
         return cam.read()
@@ -58,24 +53,26 @@ class CameraFeed:
         b = cv2.getTrackbarPos('B', 'Attributes')
 
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        #lower = np.array([70, 14, 213])
         lower = np.array([r, g, b])
         upper = np.array([255, 255, 255])
         mask = cv2.inRange(hsv, lower, upper)
         frame = cv2.bitwise_and(image, image, mask=mask)
         contours = self.get_contours(image=frame)
 
-        min_size = 15
+        max_size = 100
+        min_size = 1
 
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
 
-            if w <= min_size and h <= min_size:
+            if w <= max_size and h <= max_size and w > min_size and h > min_size:
                 cropped_image = frame[y:y+h, x:x+w]
                 gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
                 mean = np.mean(gray)
-                max_bright = {'max': mean, 'pos': (x, y, w, h)}
-                trackers.append(max_bright)
+
+                if mean > 40:
+                    max_bright = {'max': mean, 'pos': (x, y, w, h), 'con': contour}
+                    trackers.append(max_bright)
 
 
         for i in range(len(trackers)):
@@ -83,14 +80,14 @@ class CameraFeed:
                 if trackers[j]['max'] > trackers[j+1]['max']:
                     trackers[j]['max'], trackers[j+1]['max'] = trackers[j+1]['max'], trackers[j]['max']
 
-        cv2.imshow('Mask', frame)
+        #cv2.imshow('Mask', frame)
         return trackers
 
     
     def get_contours(self, image):
         edges = cv2.Canny(image, 255, 255)
         edges = cv2.GaussianBlur(edges, (3, 3), 0)
-        cv2.imshow('Edges', edges)
+        #cv2.imshow('Edges', edges)
         contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         return contours
 
